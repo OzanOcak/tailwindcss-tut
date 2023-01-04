de041080500d46eb3afcfab260a7909125ca3dad
OzanOcak <ozanocak@gmail.com>
2023-01-03 23:07:22 -0500

    media query break points

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ if we want build css file, we need to compile/build tailwind code
 ```console
 npx tailwindcss-cli build src/index.css -o  public/index.css
 ```
+
+### mediaquery break points:
+
+mobile first, maximum with is medium on mobile but sm is breakpoint after sm done, maximun width become extra large
+
+```css
+<div class="max-w-md sm:max-w-xl"></div>
+sm:bg-gray-300  md:bg-green-400 lg:bg-blue-500 xl:bg-red-400 2xl:bg-blue-500
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 function App() {
   return (
-    <div className="py-4 px-7">
+    <div className="py-4 px-7 max-w-md mx-auto sm:max-w-xl">
       <div className="flex gap-4 ">
         <img className="h-12" src="/public/logo.png" alt="logo" />
         <h1 className="text-5xl font-bold text-red-500">Santaco</h1>
       </div>
       <img
-        className="h-80 w-11/12 mt-4 rounded-lg shadow-2xl"
+        className="h-60 w-11/12 mt-4 rounded-lg shadow-2xl sm:w-full sm:object-cover
+        sm:h-[30rem] object-top"
         src="/public/img.webp"
         alt=""
       />
-      <h2 className="mt-3 font-bold text-2xl">
-        Make your christmas resolution real & you know that{" "}
-        <span className="text-red-600">Santa</span> is watching you :)
+      <h2 className="mt-3 font-bold text-2xl sm:text-3xl">
+        Make your christmas resolution real in{" "}
+        <span className="text-green-500 text-2xl sm:text-4xl">2023</span> and,
+        you know that <span className="text-red-600">Santa</span> is watching
+        you :)
       </h2>
-      <p>
+      <p className="mt-2 sm:mt-3">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus,
         laudantium? Aut cumque sint, facere quaerat, deserunt sed, maxime harum
         alias ullam doloremque dicta. Eligendi ad eaque quod voluptates omnis
